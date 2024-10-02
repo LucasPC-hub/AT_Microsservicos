b401d94b7e710c60097fd1ee1832d5b81db33b81
Lucas <lucas.pc99@hotmail.com>
2024-10-02 14:37:24 -0300

    docker added

--- Veiculos/src/main/java/edu/infnet/veiculos/controller/StatusController.java
+++ Veiculos/src/main/java/edu/infnet/veiculos/controller/StatusController.java
@@ -5,6 +5,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class StatusController {
 
@@ -14,7 +17,13 @@ public class StatusController {
     @GetMapping("/status")
     public String getStatus() {
         String serverPort = environment.getProperty("local.server.port");
-        String response = "Requisição atendida pela instância na porta: " + serverPort;
+        String ipAddress = "Unknown";
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String response = "Requisição atendida pela instância no IP: " + ipAddress + " e porta: " + serverPort;
         System.out.println(response);
         return response;
     }
